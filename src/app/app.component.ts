6a997292f08fe6c430bc8fd8226ad1e12e1a5b23
Kelly Landers <kelly.landers@aa.com>
2023-12-02 12:35:59 -0600

    Simple API call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'testProject';
+  
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(
+      (response) => {
+        console.log(response, 'test test 123');
+      }
+    )
+  }
 }

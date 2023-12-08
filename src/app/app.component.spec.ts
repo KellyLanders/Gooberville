1293c414b51b81309456ba6c63daa2b40f6f5f00
Kelly Landers <kelly.landers@aa.com>
2023-12-08 13:34:43 -0600

    Rename project - Gooberville

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testProject'`, () => {
+  it(`should have as title 'gooberVille'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('testProject');
+    expect(app.title).toEqual('gooberVille');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testProject app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('gooberVille app is running!');
   });
 });
